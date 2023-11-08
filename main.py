5afd8f6ff20e6a5bf39630286a8ac42f3d0bd134
Grigory Matveev <grigory.matveev@vega-llc.ru>
2023-11-08 16:46:28 +0300

    Автоматическое получение адреса и порта

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+from subprocess import check_output
+import re
 
 import flet as ft
 
@@ -24,6 +26,16 @@ def main(page: ft.Page):
             result_message.value = f'Ошибка отправки: {err.args}'
         page.update()
 
+    def find_host(e):
+        logs = check_output('docker logs 1bom-rabbitmq-1', shell=True)
+        match = re.search(r'(?P<ip>172\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}):(?P<port>[0-9]+)\)', logs.decode())
+        if match:
+            host.value = match.group('ip')
+            port.value = match.group('port')
+        else:
+            result_message.value = 'Не удалось извлечь информацию'
+        page.update()
+
     saved_data = defaultdict(str, saver.read_data())
     auth = ft.Text("Auth:", size=24)
     user = ft.TextField(label="User", value=saved_data['username'])
@@ -33,6 +45,7 @@ def main(page: ft.Page):
     host = ft.TextField(label="Host", value=saved_data['host'])
     port = ft.TextField(label="Port", value=saved_data['port'])
     queue = ft.TextField(label="Queue", value=saved_data['queue'])
+    find_host_button = ft.ElevatedButton(text="Get Host", on_click=find_host)
     send_button = ft.ElevatedButton(text="Submit", on_click=send_handler)
     body_json = ft.TextField(label="Body", multiline=True, value=saved_data['body'])
     result_message = ft.Text()
@@ -44,6 +57,7 @@ def main(page: ft.Page):
         body,
         host,
         port,
+        find_host_button,
         queue,
         body_json,
         send_button,
